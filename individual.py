91b918e70ff0fd7b9a6a7e3712ffe7327adbf4d3
javoxir21 <zhziedullaev@gmail.com>
2022-12-21 18:58:50 +0300

    added individual.py

---
+++ individual.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input))
+c = int(input())
+
+v = a * b * c #обьём
+s = 2 * (a * b + b * c + a * c) площадь поверхности
+
+print("обьём равен", v)
+print("Площадь поверхности равна", s)
